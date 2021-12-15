c72dc57e447756d5a50439589cb457ceccb75db6
João Pedro Figueira Amorim Parga <parga.joao@gmail.com>
2021-12-15 16:34:28 -0300

    Create neighbourhood column at cadunico input

--- 1.4-cadunico_input.R
+++ 1.4-cadunico_input.R
@@ -7,6 +7,24 @@
 
 # feito para os anos 2011:2019
 
+# https://www.mds.gov.br/webarquivos/publicacao/cadastro_unico/Manual%20do%20Entrevistador%204%20ed%20-%20Livro%20Consolidado%20-%2013042017.pdf
+# sobre coluna bairro:
+# duas variaveis contem informacoes sobre bairro no cadunico: 
+# no_localidade_fam (Quesito 1.11 - Localidade):
+  # preenchimento obrigatório; nas áreas urbanas contém o nome do bairro e nas áreas 
+  # rurais a regiao do municipio (vila, povoado, etc)
+
+# no_unidade_territorial_fam (Quesito 1.19 - Unidade Territorial Local)
+  # preenchimento nao-obrigatorio; divisao territorial definida e organizada pelo..
+  # municipio (municipio deve padronizar e organizar os codigos de cada Unidade)
+  # OBS: apesar disso, algumas observacoes (famílias) contém informação sobre bairro no
+  # unidade territorial
+
+# Qual coluna utilizar para pegar informação sobre bairro:
+  # 1- se tiver informação, utilizar no_localidade_fam
+  # 2- se no_localidade_fam não tiver informação, utilizar no_unidade_territorial_fam
+  # 3- se nenhuma tiver informação, deixar em branco
+  
 
 # 0 load setup ------------------------------------------------------------
 
@@ -27,33 +45,28 @@ f_cadunico_familia <- function(year){
   
   # * 1.1 read data ---------------------------------------------------------
 
-  # year <- 2011
-  
-  66666666666666 CRIAR COLUNA COM BAIRRO
-  DECIDIR QUAL INCLUIR:
-    no_localidade_fam
-    OU
-    no_unidade_territorial_fam
-    
-  VER MANUAL DO ENTREVISTADOR PARA ESCLARECER
+  # year <- 2019
   
   # select columns to read
-  colunas_familia <- c(
-    'co_uf',
-    'cd_ibge_cadastro',
-    'co_familiar_fam',
-    'no_tip_logradouro_fam',
-    'no_tit_logradouro_fam',
-    'no_logradouro_fam',
-    'nu_logradouro_fam',
-    'nu_cep_logradouro_fam'
+  col_familia <- c(
+    'co_uf'
+    ,'cd_ibge_cadastro'
+    ,'co_familiar_fam'
+    ,'no_tip_logradouro_fam'
+    ,'no_tit_logradouro_fam'
+    ,'no_logradouro_fam'
+    ,'nu_logradouro_fam'
+    ,'nu_cep_logradouro_fam'
+    ,"no_localidade_fam"
+    ,"no_unidade_territorial_fam"
   )
   
   # read cadunico data. 
   df_cadunico_familia <- data.table::fread(
-    input = paste0("//storage6/bases/DADOS/RESTRITO/CADASTRO_UNICO/csv/cad_familia_12", year, ".csv")
-    #, nrows = 100
-    , select = colunas_familia
+    input = paste0("//storage6/bases/DADOS/RESTRITO/CADASTRO_UNICO/csv/cad_familia_12", 
+                   year, ".csv")
+    #, nrows = 1000
+    , select = col_familia
     , colClasses = "character"
     )
   
@@ -88,7 +101,7 @@ f_cadunico_familia <- function(year){
     ,
     nu_logradouro_fam := data.table::fifelse(
       test = grepl("SN", nu_logradouro_fam),
-      yes = '',
+      yes = "",
       no = sub("^0+", "", nu_logradouro_fam)
     )
   ]
@@ -109,34 +122,70 @@ f_cadunico_familia <- function(year){
     `:=`(
       # create address columns: concatenate address components & remove excess spaces..
       #..(type of address, title address, address name, address number)
-      logradouro = trimws(gsub("\\s+", " ", paste(
-        no_tip_logradouro_fam, 
-        no_tit_logradouro_fam,
-        paste0(no_logradouro_fam, ","),
-        nu_logradouro_fam,
-        sep = " " )))
+      logradouro = trimws(
+        gsub("\\s+", " ", paste(
+          no_tip_logradouro_fam
+          , no_tit_logradouro_fam
+          , paste0(no_logradouro_fam, ",")
+          , nu_logradouro_fam
+          , sep = " " ))
+        )
+      
       , ano = as.character(year)
       
       # add "-" to CEP (A PRINCIPIO, NAO E NECESSARIO)
       #cep = gsub("^([0-9]{5})([0-9]+)$", "\\1-\\2", cadunico$nu_cep_logradouro_fam)
     )]
   
-  # create year column
-  df_cadunico_familia[, c("ano") := as.character(year)]
+  # create bairro column
+  ## vector with necessary columns
+  col_bairro <- c("no_localidade_fam", "no_unidade_territorial_fam")
+  ## remove whitespace
+  df_cadunico_familia[
+    ,
+    (col_bairro) := lapply(.SD, stringr::str_squish), 
+    .SDcols = col_bairro
+  ]
 
+
+  # replace empty no_localidade_fam with NA
+  df_cadunico_familia[
+    ,
+    no_localidade_fam := gsub("^$", NA_character_, no_localidade_fam)
+    ]
+  # replace empty no_unidade_territorial_fam with NA
+  #df_cadunico_familia[
+  #  ,
+  #  no_unidade_territorial_fam := gsub("^$", NA_character_, no_unidade_territorial_fam)
+  #]
+  
+  # bairro: 
+  # 1- no_localidade_fam if non empty (""); 
+  # 2- no_unidade_territorial_fam if no_localidade_fam empty
+  # 3- empty if both empry
+  df_cadunico_familia[
+    ,
+    bairro := data.table::fcase(
+      !is.na(no_localidade_fam), no_localidade_fam,
+      is.na(no_localidade_fam) & !is.na(no_unidade_territorial_fam), no_unidade_territorial_fam,
+      default = ""
+    )
+  ]
+  
+  
   # remove unnecessary columns
   df_cadunico_familia[
     , 
     c('no_tip_logradouro_fam','no_tit_logradouro_fam','no_logradouro_fam',
-      'nu_logradouro_fam') := NULL
+      'nu_logradouro_fam', "no_localidade_fam", "no_unidade_territorial_fam") := NULL
     ]
   
   ## make sure it is saved using UTF-8 encoding
-  colunas <- colnames(df_cadunico_familia)
+  col_encoding <- colnames(df_cadunico_familia)
   df_cadunico_familia <- df_cadunico_familia[
     ,
     lapply(.SD, enc2utf8),
-    .SDcols = colunas
+    .SDcols = col_encoding
   ]
   
   for (name in colnames(df_cadunico_familia)) {
